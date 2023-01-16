ce3d390e08a047586c62fedb820ca01f8ee3e0c8
jairalcon <98365593+jairalcon@users.noreply.github.com>
2023-01-16 17:24:48 -0600

    updating alt for img

--- components/About.jsx
+++ components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
                     <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects.</p>
                 </div>
                 <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-                    <Image className='rounded-xl' src={headShot} width='500' height='500' alt="headshot of Jair" />
+                    <Image className='rounded-xl' src={headShot} width='500' height='500' alt="headshot image" />
                 </div>
             </div>
         </div>
