02ce541a12867fd13c2359997b7bbec5a1870397
jairalcon <98365593+jairalcon@users.noreply.github.com>
2023-01-16 21:33:56 -0600

    adding getform, need to add handleSubmit

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -8,6 +8,12 @@ import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 import contactImg from '../public/assets/contact.jpg';
 
 const Contact = () => {
+
+    // function handleSubmit() {
+        //todo needs to clear form once submitted
+        //? https://docs.getform.io/guides/nextjs/
+    // }
+
     return (
         <div id='contact' className='w-full lg:h-screen'>
             <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
@@ -21,7 +27,7 @@ const Contact = () => {
                                 <Image className='rounded-xl hover:scale-105 ease-in duration-300' src={contactImg} width='700' height='700' alt="contact photo" />
                             </div>
                             <div>
-                                <h2 className='py-2'>Name here</h2>
+                                {/* <h2 className='py-2'>Name here</h2> */}
                                 <p>Front-End Developer</p>
                                 <p>I am available for freelance of full-time positions. Contact me and let&rsquo;s talk.</p>
                             </div>
@@ -63,34 +69,35 @@ const Contact = () => {
                     {/* right */}
 
                     <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
+                        <p>/ / Submit the form below or shoot me an email - jairalcon@gmail.com</p>
                         <div className='p-4'>
 
-                            <form>
+                            <form method='POST' action='https://getform.io/f/a025d003-cd9b-433e-aedc-3418d0c380f0'>
                                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                                     <div className='flex flex-col'>
                                         <label className='uppercase text-sm py-2'>Name</label>
-                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" />
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='name' />
                                     </div>
                                     <div className='flex flex-col'>
                                         <label className='uppercase text-sm py-2'>Phone Number</label>
-                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" />
+                                        <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='phone number' />
                                     </div>
                                 </div>
                                 <div className='flex flex-col py-2'>
                                     <label className='uppercase text-sm py-2'>Email</label>
-                                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="email" />
+                                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="email" name='email'/>
                                 </div>
 
                                 <div className='flex flex-col py-2'>
                                     <label className='uppercase text-sm py-2'>Subject</label>
-                                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" />
+                                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='subject'/>
                                 </div>
 
                                 <div className='flex flex-col py-2'>
                                     <label className='uppercase text-sm py-2'>Message</label>
-                                    <textarea className='border-2 rounded-lg p-3 border-gray-300' rows='10' type="text" />
+                                    <textarea className='border-2 rounded-lg p-3 border-gray-300' rows='10' type="text" name='message'/>
                                 </div>
-                                <button className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
+                                <button className='w-full p-4 text-gray-100 mt-4'>Send Message to Jair</button>
                             </form>
                         </div>
                     </div>
