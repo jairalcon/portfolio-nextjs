621564aacd6aa9743646adef03e4702d509812a8
jairalcon <98365593+jairalcon@users.noreply.github.com>
2023-01-19 11:11:35 -0600

    updating NextJS image handling

--- pages/crypto.jsx
+++ pages/crypto.jsx
@@ -1,4 +1,4 @@
-import Image from "next/legacy/image";
+import Image from "next/image";
 import React from 'react'
 import cryptoImg from '../public/assets/projects/crypto.jpg';
 import { RiRadioButtonFill } from 'react-icons/ri';
@@ -9,7 +9,15 @@ const crypto = () => {
         <div className='w-full'>
             <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
                 <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
-                <Image className='absolute z-1' layout='fill' objectFit='cover' src={cryptoImg} alt='/' />
+                <Image
+                    className='absolute z-1'
+                    src={cryptoImg}
+                    alt='/'
+                    fill
+                    sizes="100vw"
+                    style={{
+                        objectFit: "cover"
+                    }} />
                 <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
                     <h2 className='py-2'>Crypto App</h2>
                     <h3>React JS / Tailwind / Firebase</h3>
@@ -46,7 +54,7 @@ const crypto = () => {
             </div>
 
         </div>
-    )
+    );
 }
 
 export default crypto
--- pages/twitch.jsx
+++ pages/twitch.jsx
@@ -1,4 +1,4 @@
-import Image from "next/legacy/image";
+import Image from "next/image";
 import React from 'react'
 import twitchImg from '../public/assets/projects/twitch.jpg';
 import { RiRadioButtonFill } from 'react-icons/ri';
@@ -9,7 +9,15 @@ const twitch = () => {
         <div className='w-full'>
             <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
                 <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
-                <Image className='absolute z-1' layout='fill' objectFit='cover' src={twitchImg} alt='/' />
+                <Image
+                    className='absolute z-1'
+                    src={twitchImg}
+                    alt='/'
+                    fill
+                    sizes="100vw"
+                    style={{
+                        objectFit: "cover"
+                    }} />
                 <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
                     <h2 className='py-2'>Twitch App</h2>
                     <h3>React JS / Tailwind / Firebase</h3>
@@ -46,7 +54,7 @@ const twitch = () => {
             </div>
 
         </div>
-    )
+    );
 }
 
 export default twitch
