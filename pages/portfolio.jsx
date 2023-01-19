8a2d1bbf184a22427109caffd83f88a358437777
jairalcon <98365593+jairalcon@users.noreply.github.com>
2023-01-19 11:55:36 -0600

    updating component

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from 'react'
-import cryptoImg from '../public/assets/projects/crypto.jpg';
+import portfolioImg from '../public/assets/projects/portfolio.jpg';
 import { RiRadioButtonFill } from 'react-icons/ri';
 import Link from 'next/link';
 
@@ -11,7 +11,7 @@ const crypto = () => {
                 <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
                 <Image
                     className='absolute z-1'
-                    src={cryptoImg}
+                    src={portfolioImg}
                     alt='/'
                     fill
                     sizes="100vw"
@@ -19,8 +19,8 @@ const crypto = () => {
                         objectFit: "cover"
                     }} />
                 <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
-                    <h2 className='py-2'>Crypto App</h2>
-                    <h3>React JS / Tailwind / Firebase</h3>
+                    <h2 className='py-2'>Portfolio Page</h2>
+                    <h3>NextJS / Tailwind</h3>
                 </div>
             </div>
 
@@ -31,17 +31,20 @@ const crypto = () => {
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices dui sapien eget mi proin sed libero enim. Purus sit amet luctus venenatis lectus magna. Semper viverra nam libero justo laoreet sit. Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget. Elit pellentesque habitant morbi tristique. Pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada. Tortor consequat id porta nibh venenatis. Tellus at urna condimentum mattis pellentesque id nibh tortor id. Amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar.
                     </p>
-                    <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
-                    <button className='px-8 py-2 mt-4'>Code</button>
+                    <Link href='https://main.dl4v5rpi943x2.amplifyapp.com/'>
+                        <button className='px-8 py-2 mt-4 mr-8 hover:scale-110 ease-in duration-300'>Demo</button>
+                    </Link>
+                    <Link href='https://github.com/jairalcon/portfolio-nextjs'>
+                        <button className='px-8 py-2 mt-4 hover:scale-110 ease-in duration-300'>Code</button>
+                    </Link>
                 </div>
                 <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4'>
                     <div className='p-2'>
                         <p className='text-center font-bold pb-2'>Technologies</p>
                         <div className='grid grid-cols-3 md:grid-cols-1'>
-                            <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />React</p>
+                            <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />React (NextJS)</p>
                             <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />Tailwind</p>
                             <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />JavaScript</p>
-                            <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' />Firebase</p>
                         </div>
 
                     </div>
