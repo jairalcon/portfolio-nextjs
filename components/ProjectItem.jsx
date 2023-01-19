658ce379ea34d7b4fcc3c4bbe6539e8c79244189
jairalcon <98365593+jairalcon@users.noreply.github.com>
2023-01-19 11:21:20 -0600

    updating tailwind hover effedct for image

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import Link from 'next/link'
 import React from 'react'
 
-const ProjectItem = ({ title, backgroundImg, projectUrl}) => {
+const ProjectItem = ({ title, backgroundImg, projectUrl }) => {
     return (
         <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
             <Image
-                className='rounded-xl group-hover:opacity-10'
+                className='rounded-xl group-hover:opacity-10 hover:scale-105 ease-in duration-300'
                 src={backgroundImg}
                 alt='/'
                 style={{
@@ -17,7 +17,7 @@ const ProjectItem = ({ title, backgroundImg, projectUrl}) => {
                 <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
                 <p className='pb-4 pt-2 text-white text-center'>React JS</p>
                 <Link href={projectUrl} >
-                    <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold tx-lg cursor-pointer'>More Info</p>
+                    <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold tx-lg cursor-pointer hover:scale-110 ease-in duration-300'>More Info</p>
                 </Link>
             </div>
         </div>
